40501d0c50814026a9fe67a5cc5e309f9bbba631
Skye Russ <picklep@clubruss.org>
2025-10-22 09:03:00 -0700

    100 iterations run.

--- PA2/data_collection/collect_data.sh
+++ PA2/data_collection/collect_data.sh
@@ -6,7 +6,7 @@ OUTPUT_FILENAME="pa2_data_$(date '+%Y-%m-%d_%H-%M-%S').csv"
 echo "Type, Quantum, Average Response Time, Context Switch Time" >> $OUTPUT_FILENAME
 
 ITERATION_LIMIT=100
-QUANTUM_LIMIT=50000
+QUANTUM_LIMIT=250000
 
 for iteration in $(seq 1 $ITERATION_LIMIT)
 do
@@ -15,7 +15,7 @@ do
     do
         echo "$basic, $(.\/$basic.o)" >> $OUTPUT_FILENAME
     done
-    for N in $(seq 1000 1000 $QUANTUM_LIMIT)
+    for N in $(seq 0 5000 $QUANTUM_LIMIT)
     do
         for i in mlfq rr
         do
